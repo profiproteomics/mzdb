877715e7177cd7f73d955cf365f14a58c2f3c091
Christophe Bruley <christophe.bruley@cea.fr>
2015-12-14 20:29:49 +0000

    - alternative mgf IPrecursorComputer implementation added

---
+++ mzdb-processing/src/main/scala/fr/profi/mzdb/cli/MzDbProcessing.scala
@@ -0,0 +1,85 @@
+package fr.profi.mzdb.cli
+
+import com.typesafe.scalalogging.LazyLogging
+import com.beust.jcommander.Parameter
+import com.beust.jcommander.Parameters
+import fr.profi.mzdb.io.writer.mgf.PrecursorMzComputationEnum
+import com.beust.jcommander.JCommander
+import fr.profi.util.ThreadLogger
+import fr.profi.mzdb.io.writer.mgf.MgfWriter
+import com.beust.jcommander.ParameterException
+import fr.profi.mzdb.io.writer.mgf.ProlinePrecursorMzComputer
+
+/**
+ * @author CB205360
+ */
+object MzDbProcessing extends App with LazyLogging {
+
+  @Parameters(commandNames = Array("create_mgf"), commandDescription = "Create an MGF peaklist from a mzdb file", separators = "=")
+  private object CreateMgfCommand {
+
+    @Parameter(names = Array("-mzdb", "--mzdb_file_path"), description = "mzDB file to perform extraction", required = true)
+    var mzdbFile: String = ""
+
+    @Parameter(names = Array("-o", "--output_file_path"), description = "mgf output file path", required = true)
+    var outputFile: String = ""
+
+    @Parameter(names = Array("-precmz", "--precursor_mz"), description = "must be on of 'main_precursor_mz, selected_ion_mz, extracted, refined, refined_thermo, proline'", required = false)
+    var precMzComputation: String = "main_precursor_mz"
+
+    @Parameter(names = Array("-mztol", "--mz_tol_ppm"), description = "m/z tolerance used for precursor m/z value definition", required = false)
+    var mzTolPPM: Float = 20
+
+    @Parameter(names = Array("-cutoff", "--intensity_cutoff"), description = "optional intensity cutoff to use", required = false)
+    var intensityCutoff: Float = 0f
+
+    @Parameter(names = Array("-ptitle", "--proline_title"), description = "export TITLE using the Proline convention", required = false)
+    var exportProlineTitle: Boolean = false
+  }
+
+  override def main(args: Array[String]): Unit = {
+    Thread.currentThread.setUncaughtExceptionHandler(new ThreadLogger(logger.underlying.getName()))
+    val jCmd = new JCommander()
+    jCmd.addCommand(CreateMgfCommand)
+
+    var parsedCommand = ""
+
+    try {
+      jCmd.parse(args: _*)
+      parsedCommand = jCmd.getParsedCommand()
+      if (parsedCommand.equals("create_mgf")) {
+        createMgf();
+      }
+    } catch {
+
+      case pEx: ParameterException => {
+        logger.warn("Invalid command or parameter", pEx)
+        jCmd.usage()
+      }
+
+      case ex: Exception => {
+        logger.error("Execution of command '" + parsedCommand + "' failed", ex)
+        jCmd.usage()
+      }
+
+    }
+  }
+
+  def createMgf(): Unit = {
+
+    logger.info("Creating MGF File for mzDB at: " + CreateMgfCommand.mzdbFile);
+    logger.info("Precursor m/z values will be defined using the method: " + CreateMgfCommand.precMzComputation);
+
+    val writer = new MgfWriter(CreateMgfCommand.mzdbFile);
+    val precCompEnum = PrecursorMzComputationEnum.values().find(_.name() == CreateMgfCommand.precMzComputation)
+    
+    if (precCompEnum.isDefined) { 
+       writer.write(CreateMgfCommand.outputFile,  precCompEnum.get, CreateMgfCommand.mzTolPPM, CreateMgfCommand.intensityCutoff, CreateMgfCommand.exportProlineTitle);
+    } else if (CreateMgfCommand.precMzComputation == "proline") {
+       val precComputer = new ProlinePrecursorMzComputer(writer.getMzDbReader, CreateMgfCommand.mzTolPPM)
+       writer.write(CreateMgfCommand.outputFile, precComputer, CreateMgfCommand.intensityCutoff, CreateMgfCommand.exportProlineTitle);
+    } else {
+      throw new IllegalArgumentException
+    }
+  }
+}
---
+++ mzdb-processing/src/main/scala/fr/profi/mzdb/io/writer/mgf/ProlinePrecursorMzComputer.scala
@@ -0,0 +1,64 @@
+package fr.profi.mzdb.io.writer.mgf
+
+import fr.profi.mzdb.MzDbReader
+import fr.profi.mzdb.model.SpectrumHeader
+import fr.profi.mzdb.algo.IsotopicPatternScorer
+import fr.profi.ms.model.TheoreticalIsotopePattern
+import com.typesafe.scalalogging.LazyLogging
+
+/**
+ * @author CB205360
+ */
+
+class ProlinePrecursorMzComputer(reader: MzDbReader, mzTolPPM: Float)  extends IPrecursorComputation with LazyLogging {
+
+  private var lastPrediction: (SpectrumHeader, TheoreticalIsotopePattern) = _
+  
+  override def getPrecursorMz(spectrumHeader: SpectrumHeader): Double = {
+    val time = spectrumHeader.getElutionTime()
+    var precMz = spectrumHeader.getPrecursorMz()
+    val bestPattern = getBestIsotopicPatternMatch(spectrumHeader, precMz, time)
+    if (bestPattern.isDefined) {
+      lastPrediction = (spectrumHeader, bestPattern.get)
+      if (math.abs(precMz - bestPattern.get.monoMz) > 1e-3)
+        logger.info(s"change predicted precursorMz from $precMz to ${bestPattern.get.monoMz}")
+      bestPattern.get.monoMz
+    } else {
+      logger.info("no prediction : returns precursorMz")
+      precMz
+    }
+  }
+
+  private def getBestIsotopicPatternMatch(spectrumHeader: SpectrumHeader, precMz: Double, time: Float): Option[TheoreticalIsotopePattern] = {
+    val slices = reader.getMsSpectrumSlices(precMz - 5, precMz + 5, time-5f, time+5f)
+    if (!slices.isEmpty) {
+      val slice = slices.minBy { x => Math.abs(x.getHeader.getElutionTime-time) }
+      val putativePatterns = IsotopicPatternScorer.calclIsotopicPatternHypotheses(slice.getData(), precMz, mzTolPPM)
+      Some(putativePatterns.head._2)
+    } else None
+  }
+
+  override def getPrecursorCharge(spectrumHeader: SpectrumHeader): Int = {
+    val charge = spectrumHeader.getPrecursorCharge
+    if ((lastPrediction != null) && (spectrumHeader == lastPrediction._1)) {
+       if (charge !=  lastPrediction._2.charge)
+        logger.info(s"change predicted charge from $charge to ${lastPrediction._2.charge}")
+      lastPrediction._2.charge
+    } else {
+      val time = spectrumHeader.getElutionTime()
+      var precMz = spectrumHeader.getPrecursorMz()
+      val bestPattern = getBestIsotopicPatternMatch(spectrumHeader, precMz, time)
+      if (bestPattern.isDefined) {
+       if (charge !=  bestPattern.get.charge)
+        logger.info(s"Change predicted charge from $charge to ${bestPattern.get.charge}")
+        bestPattern.get.charge
+      } else {
+        charge
+      }
+    }
+  }
+  
+  override def  getParamName(): String =  {
+    "Proline refined precursor mz"
+  }
+}
