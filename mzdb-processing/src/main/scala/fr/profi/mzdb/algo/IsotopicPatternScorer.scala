928ee92aa4a8bd95db69b99b45bb08fb7ecccb50
David Bouyssié <David.Bouyssie@ipbs.fr>
2016-06-09 13:22:51 +0000

    Remove obsolete code and tries to stimulate Jenkins ;-) (c'est pas l'heure de la sieste !)

--- mzdb-processing/src/main/scala/fr/profi/mzdb/algo/IsotopicPatternScorer.scala
+++ mzdb-processing/src/main/scala/fr/profi/mzdb/algo/IsotopicPatternScorer.scala
@@ -88,67 +88,6 @@ object IsotopicPatternScorer extends LazyLogging {
       .toArray
   }
   
-  /*
-  // TODO: remove me => this method seems to not be used
-
-  /**
-   * Scores a single isotopic pattern hypothesis. The score is based on the relative difference between predicted and
-   * observed intensities. In case a theoretical peak is not found in the MS data a penality is applied to the difference.
-   * The penality is less important as the rank of the isotope peak is high.
-   *
-   * @param spectrum The MS data (mz, intensities) signal around the peak to explain.
-   * @param mz The mz of the peak that must be explained.
-   * @param charge The isotope pattern charge state.
-   * @param ppmTol
-   * @return The score and the theoretical isotope pattern.
-   */
-   private def calcIsotopicPatternHypothesis(
-    spectrum: SpectrumData,
-    mz: Double,
-    charge: Int,
-    ppmTol: Double
-  ): (Double, TheoreticalIsotopePattern) = {
-    
-    this.calcIsotopicPatternHypothesis(
-      spectrum,
-      mz,
-      charge,
-      ppmTol,
-      IsotopePatternEstimator.getTheoreticalPattern(mz, charge),
-      rank => Math.max(0.01, Math.pow(10, -(2 * rank - 4))) // Math.min(100.0, 0.0001 * Math.pow(10, rank*2))
-    )
-    
-    /*var score = 0.0
-    val pattern = IsotopePatternEstimator.getTheoreticalPattern(mz, charge)
-    var ipMoz = mz
-    val isotopeAbundance = spectrum.getIntensityList()(spectrum.getNearestPeakIndex(ipMoz))
-    val normAbundance = pattern.mzAbundancePairs(0)._2
-    var rank = 0
-    var matches = 0
-    
-    for (rank <- 0 until pattern.mzAbundancePairs.length) {
-      ipMoz = if (rank == 0) ipMoz else ipMoz + IsotopePatternEstimator.avgIsoMassDiff / charge
-      val ipAbundance = (pattern.mzAbundancePairs(rank)._2) * isotopeAbundance / normAbundance
-      var nearestPeakIdx = spectrum.getNearestPeakIndex(ipMoz)
-      val penality = Math.max(0.01, Math.pow(10, -(2 * rank - 4))) // Math.min(100.0, 0.0001 * Math.pow(10, rank*2));
-      val abundance = {
-        if ((1e6 * Math.abs(spectrum.getMzList()(nearestPeakIdx) - ipMoz) / ipMoz) < ppmTol) {
-          ipMoz = spectrum.getMzList()(nearestPeakIdx)
-          matches += 1
-          spectrum.getIntensityList()(nearestPeakIdx)
-        } else {
-          ipAbundance / 1000.0f
-        }
-      }
-      val d = ((ipAbundance - abundance) / math.min(abundance, ipAbundance)) * penality
-      score += d * d
-    }
-
-    score = Math.log10(score) - matches
-    
-    (score, pattern)*/
-  }*/
-
   /**
    * Scores a single isotopic pattern hypothesis. The score is based on the relative difference between predicted and
    * observed intensities. In case a theoretical peak is not found in the MS data a penality is applied to the difference.
