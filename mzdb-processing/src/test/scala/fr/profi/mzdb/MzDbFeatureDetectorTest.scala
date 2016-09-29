5d29f1ea6c03bd51a6760cf10da1630de5ca7bb9
David Bouyssié <David.Bouyssie@ipbs.fr>
2016-09-29 12:20:59 +0000

    Apply scalariform to JUnit test

--- mzdb-processing/src/test/scala/fr/profi/mzdb/MzDbFeatureDetectorTest.scala
+++ mzdb-processing/src/test/scala/fr/profi/mzdb/MzDbFeatureDetectorTest.scala
@@ -15,42 +15,44 @@ import fr.profi.mzdb.model.PutativeFeature
 class MzDbFeatureDetectorTest extends StrictLogging {
 
   var mzDb: MzDbReader = _
-  
+
   @Before
   @throws(classOf[Exception])
   def setUp() = {
-     mzDb = new MzDbReader(MzDbFeatureDetectorTest.this.getClass.getResource("/OVEMB150205_12.raw.mzDB").getFile(), true ) 
+    mzDb = new MzDbReader(MzDbFeatureDetectorTest.this.getClass.getResource("/OVEMB150205_12.raw.mzDB").getFile(), true)
   }
-  
+
   @Test
   def testDetectPeakels() = {
-      try {
-        this.logger.debug("detecting peakels in raw MS survey...")
-        val mzdbFtDetector = new MzDbFeatureDetector(
-          mzDb,
-          FeatureDetectorConfig(
-            msLevel = 1,
-            mzTolPPM = 5.0f,
-            minNbOverlappingIPs = 5
-          )
+    try {
+      this.logger.debug("detecting peakels in raw MS survey...")
+      val mzdbFtDetector = new MzDbFeatureDetector(
+        mzDb,
+        FeatureDetectorConfig(
+          msLevel = 1,
+          mzTolPPM = 5.0f,
+          minNbOverlappingIPs = 5
         )
-        
-        val runSliceIterator = new LcMsRunSliceIterator(mzDb, mzDb.getConnection(), 450, 452)
-        val detectedPeakels = mzdbFtDetector.detectPeakels(runSliceIterator)
-        logger.info("# of peakels detected : "+detectedPeakels.length)
-        for( peakel <- detectedPeakels ) {
+      )
+
+      val runSliceIterator = new LcMsRunSliceIterator(mzDb, mzDb.getConnection(), 450, 452)
+      val detectedPeakels = mzdbFtDetector.detectPeakels(runSliceIterator)
+      logger.info("# of peakels detected : " + detectedPeakels.length)
+      
+      /*for (peakel <- detectedPeakels) {
         val intensities = peakel.getIntensityValues
         val apexPeakelIdx = peakel.apexIndex
-      }        
-      } finally {
-        mzDb.close()
-      }    
+      }*/
+      
+    } finally {
+      mzDb.close()
+    }
   }
-  
-   @Test
+
+  @Test
   def testExtractFeaturesFromMS2() = {
 
-      val mzDbFts = try {
+    val mzDbFts = try {
 
       val ftXtractConfig = FeatureExtractorConfig(
         mzTolPPM = 5.0f
@@ -65,13 +67,13 @@ class MzDbFeatureDetectorTest extends StrictLogging {
 
       this.logger.debug("building a single putative features from MS2 spectrum events...")
       pfs += new PutativeFeature(
-            id = PutativeFeature.generateNewId,
-            mz = ms2SpectrumHeaders(10).getPrecursorMz,
-            charge = ms2SpectrumHeaders(10).getPrecursorCharge,
-            spectrumId = ms2SpectrumHeaders(10).getId,
-            evidenceMsLevel = 2
-          )
-      
+        id = PutativeFeature.generateNewId,
+        mz = ms2SpectrumHeaders(10).getPrecursorMz,
+        charge = ms2SpectrumHeaders(10).getPrecursorCharge,
+        spectrumId = ms2SpectrumHeaders(10).getId,
+        evidenceMsLevel = 2
+      )
+
       // Instantiates a Run Slice Data provider
       val rsdProv = new RunSliceDataProvider(mzDb.getLcMsRunSliceIterator())
 
@@ -81,7 +83,7 @@ class MzDbFeatureDetectorTest extends StrictLogging {
     } finally {
       mzDb.close()
     }
-    
+
   }
-  
+
 }
