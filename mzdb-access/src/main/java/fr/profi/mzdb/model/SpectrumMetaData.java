6c26efddd67c7896da75d971dd40acc08f9acf63
JM235353 <jean-philippe.menetrey@cea.fr>
2023-09-14 14:53:59 +0200

    - added serialization (forgotten for this class)

--- mzdb-access/src/main/java/fr/profi/mzdb/model/SpectrumMetaData.java
+++ mzdb-access/src/main/java/fr/profi/mzdb/model/SpectrumMetaData.java
@@ -1,5 +1,11 @@
 package fr.profi.mzdb.model;
 
+import fr.profi.mzdb.serialization.SerializationInterface;
+import fr.profi.mzdb.serialization.SerializationReader;
+import fr.profi.mzdb.serialization.SerializationWriter;
+
+import java.io.IOException;
+
 /**
  * Contains only few properties of Spectrum, in String format
  *  spectrumId: Long,  =SpectrumHeader.id
@@ -8,12 +14,12 @@ package fr.profi.mzdb.model;
  *  precursorList: Option[String], = SpectrumHeader.precursor
  *
  */
-public class SpectrumMetaData {
+public class SpectrumMetaData implements SerializationInterface {
 
-  protected final Long spectrumId;
-  protected final String paramTree;
-  protected final String scanList;
-  protected final String precursorList;
+  protected Long spectrumId;
+  protected String paramTree;
+  protected String scanList;
+  protected String precursorList;
 
 
   public SpectrumMetaData(Long spectrumId, String paramTree, String scanList, String precursorList) {
@@ -37,5 +43,64 @@ public class SpectrumMetaData {
 
   public String getPrecursorList() {
     return precursorList;
+  }
+
+  @Override
+  public void write(SerializationWriter writer) throws IOException {
+    boolean hasData = spectrumId!=null;
+    writer.writeBoolean(hasData);
+    if (hasData) {
+      writer.writeInt64(spectrumId);
+    }
+
+    hasData = paramTree!=null;
+    writer.writeBoolean(hasData);
+    if (hasData) {
+      writer.writeString(paramTree);
+    }
+
+    hasData = scanList!=null;
+    writer.writeBoolean(hasData);
+    if (hasData) {
+      writer.writeString(scanList);
+    }
+
+    hasData = precursorList!=null;
+    writer.writeBoolean(hasData);
+    if (hasData) {
+      writer.writeString(precursorList);
+    }
+  }
+
+
+  @Override
+  public void read(SerializationReader reader) throws IOException {
+    boolean hasData = reader.readBoolean();
+    if (hasData) {
+      spectrumId = reader.readInt64();
+    } else {
+      spectrumId = null;
+    }
+
+    hasData = reader.readBoolean();
+    if (hasData) {
+      paramTree = reader.readString();
+    } else {
+      paramTree = null;
+    }
+
+    hasData = reader.readBoolean();
+    if (hasData) {
+      scanList = reader.readString();
+    } else {
+      scanList = null;
+    }
+
+    hasData = reader.readBoolean();
+    if (hasData) {
+      precursorList = reader.readString();
+    } else {
+      precursorList = null;
+    }
   }
 }
