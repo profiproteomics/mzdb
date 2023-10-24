581fe0f80d9bd1534bbb6254ee6e1cfaebdba44f
JM235353 <jean-philippe.menetrey@cea.fr>
2023-10-24 11:14:42 +0200

    - simplified and sped up code

--- mzdb-access/src/main/java/fr/profi/mzdb/io/writer/RunSliceStructureFactory.java
+++ mzdb-access/src/main/java/fr/profi/mzdb/io/writer/RunSliceStructureFactory.java
@@ -4,15 +4,15 @@ import fr.profi.mzdb.BBSizes;
 import fr.profi.mzdb.model.RunSliceHeader;
 
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 
 public class RunSliceStructureFactory {
 
   private final BBSizes bbSizes;
   private final int maxMS1SlicesHint;
+
   private HashMap<RunSliceKey, RunSliceHeader> runSlicesStructure = new HashMap<>();
   private HashMap<Integer, RunSliceHeader> runSliceById = new HashMap<>();
+
   private final int runId;
 
   public RunSliceStructureFactory(Integer runId, int maxMS1SlicesHint, BBSizes bbSizes){
@@ -22,29 +22,43 @@ public class RunSliceStructureFactory {
   }
 
   public RunSliceHeader addRunSlice(Integer msLevel, Float beginMz, Float endMz){
+
+    //System.out.println("addRunSlice nbSlices="+runSliceById.size());
+
     int offset = (msLevel == 1) ? 1 : maxMS1SlicesHint + 1;
     int runSliceId = (int) (Math.floor(beginMz / bbSizes.BB_MZ_HEIGHT_MS1)) + offset;
     RunSliceHeader runSlice = new RunSliceHeader(runSliceId, msLevel, -1, beginMz, endMz, runId);
-    fr.profi.mzdb.io.writer.RunSliceStructureFactory.RunSliceKey key = new RunSliceKey(msLevel, beginMz,endMz);
+
+    RunSliceKey key = new RunSliceKey(msLevel, beginMz,endMz);
     runSlicesStructure.put(key, runSlice);
     runSliceById.put(runSlice.getId(),runSlice);
+
     return runSlice;
   }
 
   public boolean hasRunSlice(Integer msLevel, Float beginMz, Float endMz) {
-    return hasRunSlice(new RunSliceKey(msLevel, beginMz, endMz));
+
+    _key.msLevel = msLevel;
+    _key.beginMz = beginMz;
+    _key.endMz = endMz;
+    return hasRunSlice(_key);
   }
+  private RunSliceKey _key = new RunSliceKey();
 
   public boolean hasRunSlice(RunSliceKey key) {
     return runSlicesStructure.containsKey( key);
   }
 
   public Integer getRunSliceId(Integer msLevel, Float beginMz, Float endMz) {
-    RunSliceKey k = new RunSliceKey(msLevel, beginMz,endMz);
-    if(hasRunSlice(k))
-      return runSlicesStructure.get(k).getId();
-    else
-      return null;
+
+    _key.msLevel = msLevel;
+    _key.beginMz = beginMz;
+    _key.endMz = endMz;
+    if (hasRunSlice(_key)) {
+      return runSlicesStructure.get(_key).getId();
+    }
+
+    return null;
   }
 
   public RunSliceHeader getRunSlice(Integer id){
@@ -52,24 +66,32 @@ public class RunSliceStructureFactory {
   }
 
   public List<RunSliceHeader> getAllRunSlices() {
-    AtomicInteger finalRunSliceNumber = new AtomicInteger(1);
-    List<RunSliceHeader> headers = new ArrayList<>();
-    Map<Integer, List<RunSliceHeader>> runSlicesHeaderByMsLevel = runSlicesStructure.values().stream().collect(Collectors.groupingBy(RunSliceHeader::getMsLevel));
-    List<Integer> msLevels = runSlicesHeaderByMsLevel.keySet().stream().collect(Collectors.toList());
-    Collections.sort(msLevels);
-    for(Integer msL : msLevels){
-      List<RunSliceHeader> currentHeaders = runSlicesHeaderByMsLevel.get(msL);
-      currentHeaders.sort(Comparator.comparing(o -> Integer.valueOf(o.getId())));
-      currentHeaders.forEach(rsH ->{
-        RunSliceHeader newRsH = new RunSliceHeader(rsH.getId(), rsH.getMsLevel(), finalRunSliceNumber.getAndIncrement(), rsH.getBeginMz(), rsH.getEndMz(),rsH.getRunId());
-        headers.add(newRsH);
-      });
+
+    // Comparator used to sort according to MsLevel and id.
+    Comparator<RunSliceHeader> comparator = new Comparator<>() {
+      public int compare(RunSliceHeader rsh1, RunSliceHeader rsh2) {
+        if (rsh1.getMsLevel() < rsh2.getMsLevel()) {
+          return -1;
+        }
+        if (rsh1.getMsLevel() > rsh2.getMsLevel()) {
+          return 1;
+        }
+
+        return rsh1.getId()-rsh2.getId();
+      }
+    };
+
+    // Set number to all headers in ascending msLevel/id
+    List<RunSliceHeader> headers = new ArrayList<RunSliceHeader>(runSlicesStructure.values());
+    headers.sort(comparator);
+    for (int i=0; i<headers.size(); i++) {
+      headers.get(i).setNumber(i+1);
     }
 
     return headers;
   }
 
-  public Integer getRunSlicesCount(){
+  public Integer getRunSlicesCount() {
     return runSliceById.size();
   }
 
@@ -78,6 +100,10 @@ public class RunSliceStructureFactory {
     Integer msLevel;
     Float beginMz;
     Float endMz;
+
+    public RunSliceKey() {
+
+    }
 
     public RunSliceKey(Integer msL, Float beginMz, Float endMz){
       this.msLevel = msL;
@@ -97,7 +123,7 @@ public class RunSliceStructureFactory {
       return endMz;
     }
 
-   @Override
+    @Override
     public boolean equals(Object o) {
       if (this == o) return true;
       if (o == null || getClass() != o.getClass()) return false;
